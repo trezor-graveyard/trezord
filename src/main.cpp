f153f28ba592f2bf678318f3845857a04072f20d
Jan Pochyla <jpochyla@gmail.com>
2014-06-18 18:12:25 +0200

    Add trezor_device class
    - ported from the plugin codebase
    - c++11 idioms
    - removed buffer limit on reads, uses vector effectively kept at 64 bytes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <hidapi.h>
+
 #include <queue>
 #include <mutex>
 #include <condition_variable>
@@ -7,6 +9,130 @@
 #include <iostream>
 #include <string>
 #include <regex>
+
+namespace hid
+{
+    struct trezor_device
+    {
+        typedef unsigned char char_type;
+        typedef std::size_t size_type;
+
+        typedef std::runtime_error open_error;
+        typedef std::runtime_error read_error;
+        typedef std::runtime_error write_error;
+
+        trezor_device(const trezor_device&) = delete;
+        trezor_device &operator=(const trezor_device&) = delete;
+
+        trezor_device(const char *path)
+        {
+            hid = hid_open_path(path);
+            if (!hid) {
+                throw new open_error("HID device open failed");
+            }
+
+            hid_set_nonblocking(hid, 0); // always block on read
+
+            unsigned char uart[] = {0x41, 0x01}; // enable UART
+            unsigned char txrx[] = {0x43, 0x03}; // purge TX/RX FIFOs
+            hid_send_feature_report(hid, uart, 2);
+            hid_send_feature_report(hid, txrx, 2);
+        }
+
+        ~trezor_device()
+        {
+            hid_close(hid);
+        }
+
+        void
+        read_buffered(char_type *data, size_type len)
+        {
+            if (read_buffer.empty()) {
+                buffer_report();
+            }
+            size_type n = read_report_from_buffer(data, len);
+            if (n < len) {
+                read_buffered(data + n, len - n);
+            }
+        }
+
+        void
+        write(const char_type *data, size_type len)
+        {
+            size_type n = write_report(data, len);
+            if (n < len) {
+                write(data + n, len - n);
+            }
+        }
+
+    private:
+
+        void
+        buffer_report()
+        {
+            report_type report;
+            int r = hid_read(hid,
+                             report.data(),
+                             report.size());
+
+            if (r < 0) {
+                throw new read_error("HID device read failed");
+            }
+            if (r > 0) {
+                // copy to the buffer, skip the report number
+                char_type rn = report[0];
+                size_type n = std::min(static_cast<size_type>(rn),
+                                       static_cast<size_type>(r - 1));
+                std::copy(report.begin() + 1,
+                          report.begin() + n,
+                          std::back_inserter(read_buffer));
+            }
+        }
+
+        size_type
+        read_report_from_buffer(char_type *data, size_type len)
+        {
+            size_type n = std::min(read_buffer.size(), len);
+            auto r1 = read_buffer.begin();
+            auto r2 = read_buffer.begin() + n;
+
+            std::copy(r1, r2, data); // copy to data
+            read_buffer.erase(r1, r2); // shift from buffer
+
+            return n;
+        }
+
+        size_type
+        write_report(const char_type *data, size_type len)
+        {
+            report_type report;
+            report[0] = report.size() - 1; // rest is 0x00
+
+            size_type n = std::min(report.size() - 1, len);
+            std::copy(data,
+                      data + n,
+                      report.begin() + 1); // copy behind report number
+
+            int r = hid_write(hid,
+                              report.data(),
+                              report.size());
+            if (r < 0) {
+                throw new write_error("HID device write failed");
+            }
+            if (r < report.size()) {
+                throw new write_error("HID device write was insufficient");
+            }
+
+            return n;
+        }
+
+        typedef std::vector<char_type> buffer_type;
+        typedef std::array<char_type, 64> report_type;
+
+        hid_device *hid;
+        buffer_type read_buffer;
+    };
+}
 
 namespace core
 {
