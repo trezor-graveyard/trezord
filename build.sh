731ba6f494d8803d803e4b22267935b60f543c94
Pavol Rusnak <stick@gk2.sk>
2014-07-15 18:43:24 +0200

    release process work-in-progress

--- build.sh
+++ build.sh
@@ -2,6 +2,24 @@
 
 set -e
 
+cd $(dirname $0)
+
+# Compile cpp-netlib
+if [ \! -d lib/cpp-netlib ]; then
+  mkdir -p lib/cpp-netlib && cd lib/cpp-netlib
+  cmake ../../vendor/cpp-netlib
+  make
+  cd ../..
+fi
+
+# Compile jsoncpp
+if [ \! -d lib/jsoncpp ]; then
+  mkdir -p lib/jsoncpp && cd lib/jsoncpp
+  cmake ../../vendor/jsoncpp
+  make
+  cd ../..
+fi
+
 mkdir -p build && cd build
 cmake ..
 make
