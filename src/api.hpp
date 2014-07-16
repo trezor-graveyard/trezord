d0d42f18130f087130fcfe5e07a05ebb5bf9e780
Jan Pochyla <jpochyla@gmail.com>
2014-07-16 17:02:08 +0200

    Add enumeration, configuration, and boost logging
    - added body-reading middleware, investigating data corruption bug
    - kernels moved to trezord::core

--- src/api.hpp
+++ src/api.hpp
@@ -1,10 +1,6 @@
-#include "protobuf/json_codec.hpp"
-#include "protobuf/wire_codec.hpp"
-
 #include <boost/network/include/http/server.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
+
+#include <boost/log/trivial.hpp>
 
 #include <functional>
 #include <iostream>
@@ -17,213 +13,110 @@ namespace trezord
 namespace api
 {
 
-// loosely based on:
-// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3562.pdf
-struct async_executor
-{
-    async_executor()
-        : io_service(),
-          io_work(io_service),
-          // cannot use std::bind here, see:
-          // https://stackoverflow.com/questions/9048119
-          thread(boost::bind(&io_service_type::run, &io_service))
-    {}
-
-    ~async_executor()
-    {
-        io_service.stop();
-        thread.join();
-    }
-
-    void
-    add(std::function<void()> const &closure)
-    { io_service.post(closure); }
-
-private:
-
-    typedef boost::asio::io_service io_service_type;
-
-    io_service_type io_service;
-    io_service_type::work io_work;
-    std::thread thread;
-};
-
-struct device_kernel
-{
-    typedef std::string device_path_type;
-
-    device_path_type device_path;
-
-    device_kernel(device_path_type const &dp)
-        : device_path(dp)
-    {}
-
-    void
-    open()
-    {
-        auto ptr = new wire::device(device_path);
-        device.reset(ptr);
-    }
-
-    void
-    close()
-    { device.reset(); }
-
-    void
-    call(wire::message const &msg_in,
-         wire::message &msg_out)
-    {
-        if (!device.get()) {
-            open();
-        }
-        msg_in.write_to(*device);
-        msg_out.read_from(*device);
-    }
-
-private:
-
-    typedef std::unique_ptr<
-        wire::device
-        > wire_device_ptr_type;
-
-    wire_device_ptr_type device;
-};
-
-struct app_kernel
-{
-    typedef std::string session_id_type;
-    typedef std::string device_path_type;
-
-    std::mutex mutex;
-
-    std::map<
-        device_path_type,
-        session_id_type
-        > sessions;
-
-    std::map<
-        device_path_type,
-        device_kernel
-        > device_kernels;
-
-    std::map<
-        device_path_type,
-        async_executor
-        > device_executors;
-
-    async_executor enumeration_executor;
-
-    protobuf::state pb_state;
-    protobuf::wire_codec pb_wire_codec;
-    protobuf::json_codec pb_json_codec;
-
-    app_kernel()
-        : sessions(),
-          device_kernels(),
-          device_executors(),
-          enumeration_executor(),
-          pb_state(),
-          pb_wire_codec(pb_state),
-          pb_json_codec(pb_state)
-    {}
-
-    device_kernel *
-    get_device_kernel(device_path_type const &device_path)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        auto kernel_r = device_kernels.emplace(
-            std::piecewise_construct,
-            std::forward_as_tuple(device_path),
-            std::forward_as_tuple(device_path));
-        return &kernel_r.first->second;
-    }
-
-    async_executor *
-    get_device_executor(device_path_type const &device_path)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        auto executor_r = device_executors.emplace(
-            std::piecewise_construct,
-            std::forward_as_tuple(device_path),
-            std::forward_as_tuple());
-        return &executor_r.first->second;
-    }
-
-    session_id_type
-    acquire_session(device_path_type const &device_path)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        auto session_id = generate_session_id();
-        sessions[device_path] = session_id;
-        return session_id;
-    }
-
-    void
-    release_session(session_id_type const &session_id)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        auto session_it = find_session_by_id(session_id);
-        if (session_it != sessions.end()) {
-            sessions.erase(session_it);
-        }
-    }
-
-    decltype(sessions)::iterator
-    find_session_by_id(session_id_type const &session_id)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        return std::find_if(
-            sessions.begin(),
-            sessions.end(),
-            [&] (decltype(sessions)::value_type const &kv) {
-                return kv.second == session_id;
-            });
-    }
-
-private:
-
-    boost::uuids::random_generator uuid_generator;
-
-    session_id_type
-    generate_session_id()
-    {
-        return boost::lexical_cast<
-            session_id_type
-            >(uuid_generator());
-    }
-};
-
 struct handler
 {
     typedef boost::network::http::async_server<handler> server;
+
+    struct connection_reader
+    {
+        std::size_t stream_size;
+        std::stringstream stream;
+
+        connection_reader(std::size_t max_size_,
+                          server::connection_ptr connection_,
+                          std::function<void(connection_reader *)> callback_)
+            : stream_size(0),
+              stream(),
+              max_size(max_size_),
+              connection(connection_),
+              callback(callback_)
+        {}
+
+        void
+        read()
+        {
+            connection->read(
+                [&] (boost::iterator_range<char const *> input_range,
+                     boost::system::error_code error_code,
+                     std::size_t size,
+                     server::connection_ptr connection_)
+                {
+                    if (error_code.value() > 0) {
+                        BOOST_LOG_TRIVIAL(fatal) << "Read error";
+                    }
+
+                    stream.write(input_range.begin(), size);
+                    stream_size += size;
+
+                    if (stream_size < max_size) {
+                        read();
+                    } else {
+                        callback(this);
+                    }
+                });
+        }
+
+    private:
+
+        std::size_t max_size;
+        server::connection_ptr connection;
+        std::function<void(connection_reader *)> callback;
+    };
 
     void
     operator()(server::request const &request,
                server::connection_ptr connection)
     {
-        connection->read(
-            [&] (boost::iterator_range<char const *> input_range,
-                 boost::system::error_code error_code,
-                 std::size_t size,
-                 server::connection_ptr connection)
+        BOOST_LOG_TRIVIAL(info) << "Accepted request";
+
+        connection_reader *reader = new connection_reader(
+            read_content_length(request),
+            connection,
+            [=] (connection_reader *reader_)
             {
                 try {
-                    request.body = boost::copy_range<std::string>(input_range);
                     action_handler ahandler;
                     action_params params;
+
+                    request.body = reader_->stream.str();
+                    delete reader_;
 
                     dispatch(request, ahandler, params);
                     ahandler(this, params, request, connection);
                 }
                 catch (std::exception const &e) {
-                    connection->set_status(server::connection::internal_server_error);
-                    connection->set_headers(json_headers);
-                    connection->write(json_string({{"error", e.what()}}));
+                    try {
+                        connection->set_status(server::connection::internal_server_error);
+                        connection->set_headers(json_headers);
+                        connection->write(json_string({{"error", e.what()}}));
+                    } catch (...) {
+                        // do nothing, just dont crash the server please
+                    }
                 }
             });
+
+        reader->read();
     }
 
 private:
+
+    std::size_t
+    read_content_length(server::request const &request)
+    {
+        auto content_length_header_it = std::find_if(
+            request.headers.begin(),
+            request.headers.end(),
+            [&] (server::request::header_type const &header) {
+                return header.name == "Content-Length";
+            });
+        if (content_length_header_it == request.headers.end()) {
+            return 0;
+        }
+
+        return boost::lexical_cast<
+            std::size_t
+        >(content_length_header_it->value);
+    }
 
     typedef std::smatch action_params;
     typedef std::function<
@@ -257,7 +150,7 @@ private:
         { ".*",   ".*",            &handler::handle_404 },
     };
 
-    app_kernel k;
+    core::kernel k;
 
     // json support
 
@@ -279,7 +172,7 @@ private:
     {
         Json::Value json(Json::objectValue);
 
-        for (auto &kv: list) {
+        for (auto const &kv: list) {
             json[kv.first] = kv.second;
         }
 
@@ -305,7 +198,7 @@ private:
              action_handler &handler,
              action_params &params)
     {
-        for (auto &route: action_routes) {
+        for (auto const &route: action_routes) {
             bool m = std::regex_match(request.method, route.method);
             bool d = std::regex_match(request.destination, params,
                                       route.destination);
@@ -327,33 +220,9 @@ private:
         connection->set_status(server::connection::ok);
         connection->set_headers(json_headers);
         connection->write(json_string({
-                    {"configured", false},
-                    {"version", "0.0.1"}
+                    {"configured", k.is_configured()},
+                    {"version", k.version}
                 }));
-    }
-
-    void
-    handle_enumerate(action_params const &params,
-                     server::request const &request,
-                     server::connection_ptr connection)
-    {
-        k.enumeration_executor.add(
-            [=] {
-                auto device_paths = wire::enumerate_devices(
-                    [&] (hid_device_info const *i) {
-                        return i->vendor_id == 0x534c &&
-                               i->product_id == 0x0001;
-                    });
-
-                Json::Value list(Json::arrayValue);
-                for (auto &path: device_paths) {
-                    list.append(path);
-                }
-
-                connection->set_status(server::connection::ok);
-                connection->set_headers(json_headers);
-                connection->write(json_string(list));
-            });
     }
 
     void
@@ -361,7 +230,46 @@ private:
                      server::request const &request,
                      server::connection_ptr connection)
     {
-        // TODO
+        k.configure(request.body);
+
+        connection->set_status(server::connection::ok);
+        connection->set_headers(json_headers);
+        connection->write(json_string({}));
+    }
+
+    void
+    handle_enumerate(action_params const &params,
+                     server::request const &request,
+                     server::connection_ptr connection)
+    {
+        if (!k.is_configured()) {
+            throw std::runtime_error("not configured");
+        }
+
+        k.enumeration_executor.add(
+            [=] {
+                try {
+                    auto device_paths = wire::enumerate_devices(
+                        [&] (hid_device_info const *i) {
+                            return i->vendor_id == 0x534c &&
+                            i->product_id == 0x0001;
+                        });
+
+                    Json::Value list(Json::arrayValue);
+                    for (auto const &path: device_paths) {
+                        list.append(path);
+                    }
+
+                    connection->set_status(server::connection::ok);
+                    connection->set_headers(json_headers);
+                    connection->write(json_string(list));
+                }
+                catch (std::exception const &e) {
+                    connection->set_status(server::connection::internal_server_error);
+                    connection->set_headers(json_headers);
+                    connection->write(json_string({{"error", e.what()}}));
+                }
+            });
     }
 
     void
@@ -369,6 +277,10 @@ private:
                    server::request const &request,
                    server::connection_ptr connection)
     {
+        if (!k.is_configured()) {
+            throw std::runtime_error("not configured");
+        }
+
         auto device_path = params.str(1);
 
         auto kernel = k.get_device_kernel(device_path);
@@ -396,10 +308,14 @@ private:
                    server::request const &request,
                    server::connection_ptr connection)
     {
+        if (!k.is_configured()) {
+            throw std::runtime_error("not configured");
+        }
+
         auto session_id = params.str(1);
 
         auto session_it = k.find_session_by_id(session_id);
-        if (session_it == k.sessions.end()) {
+        if (session_it == k.sessions.end()) { // TODO: race condition
             connection->set_status(server::connection::not_found);
             connection->write(json_string({{"error", "session not found"}}));
             return;
@@ -432,6 +348,10 @@ private:
                 server::request const &request,
                 server::connection_ptr connection)
     {
+        if (!k.is_configured()) {
+            throw std::runtime_error("not configured");
+        }
+
         auto session_id = params.str(1);
 
         auto session_it = k.find_session_by_id(session_id);
--- src/core.hpp
+++ src/core.hpp
@@ -1,42 +1,242 @@
-#include <queue>
-#include <mutex>
-#include <condition_variable>
+#include "protobuf/json_codec.hpp"
+#include "protobuf/wire_codec.hpp"
+#include "config/config.pb.h"
+#include "crypto.hpp"
+
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
 #include <thread>
 
 namespace trezord
 {
-namespace protobuf
+namespace core
 {
 
-template <typename I>
-struct blocking_queue
-{
-    typedef I item_type;
+static const char *signature_keys[] = {
+    // sample.key
+    //    "-----BEGIN PUBLIC KEY-----\n"
+    //    "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAENEoHRLyHw1AM5jl0pbUNgXHFldzn1jWT\n"
+    //    "/tGrlePY/vfKVVc2QM2pz0fbYUn4WEZC8DDfQ3XEhvKnsyOotsY7QQ==\n"
+    //    "-----END PUBLIC KEY-----\n",
 
-    void
-    put(const item_type &item)
+    // production keys
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE1XG38UjF5CMsOBT3d9j66vGoQhbHjVab\n"
+    "cQQf/HaKWy2BD8O7E03QJrV+ZQBSda7e9D4VX0j8EaMux5CpMxK9WA==\n"
+    "-----END PUBLIC KEY-----\n",
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEYyecDAhm5QwFx5nTK9a6sBiLbeBlNtEQ\n"
+    "nS7ZznbLM1xJDlWu4QzJASFRMuhTCX1UMu2ga3kgc713QMlM5FFssQ==\n"
+    "-----END PUBLIC KEY-----\n",
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEQ67btvfnHFY/jtLvZOyZgUglGefvT0qp\n"
+    "iyeFToxJEm1JVtMAq0X9w0zSa8hxDeCjHb323nQ1/QtJK+cKx1/eWA==\n"
+    "-----END PUBLIC KEY-----\n",
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEh3w5/XxiI34DgjXpwHXasmFjD3juuO25\n"
+    "JIcVn//t/fYEbG+LiB+kB8Skzmwo3gsZwfTinx/LxaWP/RQyo+CTig==\n"
+    "-----END PUBLIC KEY-----\n",
+    "-----BEGIN PUBLIC KEY-----\n"
+    "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEc4TFGuga3QpSOtuxhskbkG/7ZMLHZYAr\n"
+    "8m29E73xLDGegMIhOhNsjuA9eHT9IrcNaOfe5Gnez7u1EO6aRgzaRQ==\n"
+    "-----END PUBLIC KEY-----\n"
+};
+
+// loosely based on:
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3562.pdf
+struct async_executor
+{
+    async_executor()
+        : io_service(),
+          io_work(io_service),
+          // cannot use std::bind here, see:
+          // https://stackoverflow.com/questions/9048119
+          thread(boost::bind(&io_service_type::run, &io_service))
+    {}
+
+    ~async_executor()
     {
-        std::lock_guard<std::mutex> lock(mutex);
-        queue.push(item);
-        cond_var.notify_one();
+        io_service.stop();
+        thread.join();
     }
 
     void
-    take(item_type &item)
+    add(std::function<void()> const &closure)
+    { io_service.post(closure); }
+
+private:
+
+    typedef boost::asio::io_service io_service_type;
+
+    io_service_type io_service;
+    io_service_type::work io_work;
+    std::thread thread;
+};
+
+struct device_kernel
+{
+    typedef std::string device_path_type;
+
+    device_path_type device_path;
+
+    device_kernel(device_path_type const &dp)
+        : device_path(dp)
+    {}
+
+    void
+    open()
     {
-        std::lock_guard<std::mutex> lock(mutex);
-        while (queue.empty()) {
-            cond_var.wait(lock);
+        auto ptr = new wire::device(device_path);
+        device.reset(ptr);
+    }
+
+    void
+    close()
+    { device.reset(); }
+
+    void
+    call(wire::message const &msg_in,
+         wire::message &msg_out)
+    {
+        if (!device.get()) {
+            open();
         }
-        item = queue.front();
-        queue.pop();
+        msg_in.write_to(*device);
+        msg_out.read_from(*device);
     }
 
 private:
 
+    typedef std::unique_ptr<
+        wire::device
+    > wire_device_ptr_type;
+
+    wire_device_ptr_type device;
+};
+
+struct kernel
+{
+    typedef std::string session_id_type;
+    typedef std::string device_path_type;
+
+    const std::string version = "0.0.1";
+
     std::mutex mutex;
-    std::condition_variable cond_var;
-    std::queue<item_type> queue;
+
+    std::map<
+        device_path_type,
+        session_id_type
+        > sessions;
+
+    std::map<
+        device_path_type,
+        device_kernel
+        > device_kernels;
+
+    std::map<
+        device_path_type,
+        async_executor
+        > device_executors;
+
+    async_executor enumeration_executor;
+
+    Configuration configuration;
+
+    protobuf::state pb_state;
+    protobuf::wire_codec pb_wire_codec;
+    protobuf::json_codec pb_json_codec;
+
+    kernel()
+        : sessions(),
+          device_kernels(),
+          device_executors(),
+          enumeration_executor(),
+          pb_state(),
+          pb_wire_codec(pb_state),
+          pb_json_codec(pb_state)
+    {}
+
+    void
+    configure(std::string const &config_str)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        configuration.ParseFromString(config_str);
+    }
+
+    bool
+    is_configured()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        return configuration.IsInitialized();
+    }
+
+    device_kernel *
+    get_device_kernel(device_path_type const &device_path)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        auto kernel_r = device_kernels.emplace(
+            std::piecewise_construct,
+            std::forward_as_tuple(device_path),
+            std::forward_as_tuple(device_path));
+        return &kernel_r.first->second;
+    }
+
+    async_executor *
+    get_device_executor(device_path_type const &device_path)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        auto executor_r = device_executors.emplace(
+            std::piecewise_construct,
+            std::forward_as_tuple(device_path),
+            std::forward_as_tuple());
+        return &executor_r.first->second;
+    }
+
+    session_id_type
+    acquire_session(device_path_type const &device_path)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        auto session_id = generate_session_id();
+        sessions[device_path] = session_id;
+        return session_id;
+    }
+
+    void
+    release_session(session_id_type const &session_id)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        auto session_it = find_session_by_id(session_id);
+        if (session_it != sessions.end()) {
+            sessions.erase(session_it);
+        }
+    }
+
+    decltype(sessions)::iterator
+                       find_session_by_id(session_id_type const &session_id)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        return std::find_if(
+            sessions.begin(),
+            sessions.end(),
+            [&] (decltype(sessions)::value_type const &kv) {
+                return kv.second == session_id;
+            });
+    }
+
+private:
+
+    boost::uuids::random_generator uuid_generator;
+
+    session_id_type
+    generate_session_id()
+    {
+        return boost::lexical_cast<session_id_type>(uuid_generator());
+    }
 };
 
 }
