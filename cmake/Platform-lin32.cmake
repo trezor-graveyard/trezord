f4f8c35812cc3f9ac7b778cbc396ab901c818b7a
Pavol Rusnak <stick@gk2.sk>
2014-07-18 01:04:08 +0200

    simplify build.sh; extract definitions into separate Platform files

---
+++ cmake/Platform-lin32.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_FLAGS "-m32 -static-libgcc" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-m32 -static-libgcc -static-libstdc++" CACHE STRING "")
---
+++ cmake/Platform-lin64.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_FLAGS "-static-libgcc" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++" CACHE STRING "")
