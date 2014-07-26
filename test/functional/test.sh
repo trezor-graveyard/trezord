fbc6976ccde3361575861aab752636e71127d766
Pavol Rusnak <stick@gk2.sk>
2014-07-26 16:02:58 +0200

    download config.bin if not present (for test.sh)

--- test/functional/test.sh
+++ test/functional/test.sh
@@ -5,6 +5,10 @@ set -x
 
 device_index=${1-0}
 
+if [ \! -f config.bin ]; then
+  curl https://mytrezor.com/data/plugin/config_signed.bin | xxd -p -r > config.bin
+fi
+
 curl localhost:21324/
 curl -sS -X POST --data-binary @config.bin localhost:21324/configure
 path=$(curl -sS localhost:21324/enumerate | jq -r ".[$device_index].path")
