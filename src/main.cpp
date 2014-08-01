101eee16c52ac5d099b33c424167fce3d4f1c118
Jan Pochyla <jpochyla@gmail.com>
2014-08-01 20:31:36 +0200

    Add explicit signal handling and server restarting

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,20 @@
 
 #include <easylogging++.h>
 
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #include "utils.hpp"
 #include "wire.hpp"
 #include "core.hpp"
 #include "api.hpp"
 
 _INITIALIZE_EASYLOGGINGPP
+
+static const auto server_port = "21324";
+static const auto server_address = "127.0.0.1";
+static const auto sleep_time = boost::posix_time::seconds(10);
 
 void
 configure_logging()
@@ -57,27 +65,62 @@ configure_logging()
 }
 
 void
+shutdown_server(boost::system::error_code const &error,
+                int signal,
+                trezord::api::connection_handler::server &server)
+{
+    if (!error) {
+        LOG(INFO) << "signal: " << signal << ", stopping server";
+        server.stop();
+    }
+}
+
+void
 start_server()
 {
+    using namespace boost;
     using namespace trezord;
 
+    typedef api::connection_handler::server server_type;
+
     core::kernel kernel;
-    api::request_handler<
-        api::connection_handler::server> request_handler(kernel);
+
+    // http handlers
+    api::request_handler<server_type> request_handler(kernel);
     api::connection_handler connection_handler(request_handler);
 
-    auto thread_pool = boost::make_shared<
-        boost::network::utils::thread_pool>(2);
-    api::connection_handler::server::options options(connection_handler);
-    api::connection_handler::server server(
-        options
-        .thread_pool(thread_pool)
-        .reuse_address(true)
-        .address("127.0.0.1")
-        .port("21324"));
+    // thread group
+    auto threads = make_shared<thread_group>();
 
+    // io service
+    auto io_service = make_shared<asio::io_service>();
+    auto io_work = make_shared<asio::io_service::work>(ref(*io_service));
+    threads->create_thread(bind(&asio::io_service::run, io_service));
+    threads->create_thread(bind(&asio::io_service::run, io_service));
+    threads->create_thread(bind(&asio::io_service::run, io_service));
+
+    // thread pool
+    auto thread_pool = make_shared<network::utils::thread_pool>(2);
+
+    // http server
+    server_type::options options(connection_handler);
+    server_type server(
+        options
+        .reuse_address(true)
+        .io_service(io_service)
+        .thread_pool(thread_pool)
+        .address(server_address)
+        .port(server_port));
+
+    // signal handling for clear shutdown
+    asio::signal_set signals(ref(*io_service), SIGINT, SIGTERM);
+    signals.async_wait(bind(shutdown_server, _1, _2, ref(server)));
+
+    // start the server
     LOG(INFO) << "starting server";
     server.run();
+
+    // server has finished now
     LOG(INFO) << "server finished running";
 }
 
@@ -87,13 +130,21 @@ main(int argc, char *argv[])
     _START_EASYLOGGINGPP(argc, argv);
     configure_logging();
 
-    try {
-        start_server();
+    bool restart_server = false;
+
+    do {
+        try {
+            start_server();
+            restart_server = false;
+        }
+        catch (std::exception const &e) {
+            LOG(ERROR) << e.what();
+            LOG(INFO) << "sleeping for " << sleep_time;
+            boost::this_thread::sleep(sleep_time);
+            restart_server = true;
+        }
     }
-    catch (std::exception const &e) {
-        LOG(FATAL) << e.what();
-        return 1;
-    }
+    while (restart_server);
 
     return 0;
 }
