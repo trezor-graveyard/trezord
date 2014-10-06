1c900e548a08be1190303ed89fdd098afabfa031
Pavol Rusnak <stick@gk2.sk>
2014-10-06 18:24:55 +0200

    add CMAKE_BUILD_TYPE to build script

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cd $(dirname $0)
 
 TARGET=$1
 BUILDDIR=build${TARGET:+-$TARGET}
+BUILDTYPE=Release
 
 case "$TARGET" in
   lin32 | lin64 | win32 | win64 ) # cross build
@@ -19,7 +20,7 @@ esac
 # Compile cpp-netlib
 if [ \! -d $BUILDDIR/lib/cpp-netlib ]; then
   mkdir -p $BUILDDIR/lib/cpp-netlib && cd $BUILDDIR/lib/cpp-netlib
-  cmake $PLATFORM_FILE ../../../vendor/cpp-netlib
+  cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE $PLATFORM_FILE ../../../vendor/cpp-netlib
   make $JOBS
   cd ../../..
 fi
@@ -27,11 +28,11 @@ fi
 # Compile jsoncpp
 if [ \! -d $BUILDDIR/lib/jsoncpp ]; then
   mkdir -p $BUILDDIR/lib/jsoncpp && cd $BUILDDIR/lib/jsoncpp
-  cmake $PLATFORM_FILE ../../../vendor/jsoncpp
+  cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE $PLATFORM_FILE ../../../vendor/jsoncpp
   make $JOBS
   cd ../../..
 fi
 
 mkdir -p $BUILDDIR && cd $BUILDDIR
-cmake $PLATFORM_FILE ..
+cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE $PLATFORM_FILE ..
 make $JOBS
