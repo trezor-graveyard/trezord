1251ba9cb164dd4a97da3efcba45a00a840e7f8d
Jan Pochyla <jpochyla@gmail.com>
2015-02-02 15:06:36 +0100

    mac: Fix launchd file for Yosemite, add /opt/local prefix

--- release/mac/prepare.sh
+++ release/mac/prepare.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-mkdir -p "Archive/Applications/Utilities/TREZOR Bridge/"
-cp "../../build/trezord" "Archive/Applications/Utilities/TREZOR Bridge/"
+SOURCE="../../build"
+TARGET="Archive/Applications/Utilities/TREZOR_Bridge"
+TARGET_FINAL="Archive/Applications/Utilities/TREZOR Bridge"
+
+mkdir -p "$TARGET/"
+
+cp "$SOURCE/trezord" "$TARGET/trezord"
+
+dylibbundler -od -b \
+             -x "$TARGET/trezord" \
+             -d "$TARGET/libs/" \
+             -p "@executable_path/libs/"
+
+mv "$TARGET" "$TARGET_FINAL"
