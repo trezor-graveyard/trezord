d43b201fe53a221ca7d9e3f1867eb599a517e895
Jan Pochyla <jpochyla@gmail.com>
2015-05-07 11:47:47 +0200

    Comment typo

--- src/http_server.hpp
+++ src/http_server.hpp
@@ -95,7 +95,7 @@ private:
     mhd_response_from_string(char const *body)
     {
         // MHD_creae_response_from_buffer has many modes of operation,
-        // but the buffer for MHD_RESPMEM_MUST_COPY modes is effectively
+        // but the buffer for MHD_RESPMEM_MUST_COPY mode is effectively
         // const char *, so this conversion is safe
         auto body_buffer = static_cast<void *>(const_cast<char *>(body));
 
@@ -105,10 +105,6 @@ private:
 };
 
 using request_handler = std::function<response_data (request_data const &)>;
-
-/**
- * Router
- */
 
 struct regex_route
 {
