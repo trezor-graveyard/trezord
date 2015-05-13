d0ab40dcde58c00bd3023cadcb8929213fffd8e8
Jan Pochyla <jpochyla@gmail.com>
2015-05-13 16:41:37 +0200

    mac: improve the after install script

--- release/mac/installer/after-install.sh
+++ release/mac/installer/after-install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 set -x
+set -e
 
 # find out which user is running the installation
-inst_user=`who -m | cut -f 1 -d ' '`
+inst_user=`stat /dev/console | cut -f 5 -d ' '`
 
 # load the agent file into launchd with correct user
 
 agent_file=/Library/LaunchAgents/com.bitcointrezor.trezorBridge.trezord.plist
 
-if [ -f $agent_file ]; then
-    sudo -u $inst_user launchctl unload $agent_file
-fi
+set +e
+sudo -u $inst_user launchctl unload $agent_file
+set -e
 sudo -u $inst_user launchctl load $agent_file
