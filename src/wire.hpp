b43378142d34f87621474876a222e4ba77712685
Pavol Rusnak <stick@gk2.sk>
2016-05-27 14:25:34 +0200

    skip 0xff01 usage

--- src/wire.hpp
+++ src/wire.hpp
@@ -69,6 +69,11 @@ enumerate_connected_devices(F filter)
             CLOG(DEBUG, "wire.enumerate") << "skipping, invalid device";
             continue;
         }
+        // skip debug interface
+        if (i->usage_page == 0xFF01) {
+            CLOG(DEBUG, "wire.enumerate") << "skipping, debug interface";
+            continue;
+        }
         // skip fido interface
         if (i->usage_page == 0xF1D0) {
             CLOG(DEBUG, "wire.enumerate") << "skipping, fido interface";
